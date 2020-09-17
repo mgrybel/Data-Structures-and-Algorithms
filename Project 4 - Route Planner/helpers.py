7f16f427b29a0c011750a5d3218c3bb17ff273c4
mgrybel <monika.grybel@hotmail.com>
2020-09-17 21:09:21 +0100

    Update helpers.py

--- Project 4 - Route Planner/helpers.py
+++ Project 4 - Route Planner/helpers.py
@@ -7,14 +7,14 @@ from plotly.offline import init_notebook_mode, plot, iplot
 init_notebook_mode(connected=True)
 
 class Map:
-	def __init__(self, G):
-		self._graph = G
-		self.intersections = nx.get_node_attributes(G, "pos")
-		self.roads = [list(G[node]) for node in G.nodes()]
+    def __init__(self, G):
+        self._graph = G
+        self.intersections = nx.get_node_attributes(G, "pos")
+        self.roads = [list(G[node]) for node in G.nodes()]
 
-	def save(self, filename):
-		with open(filename, 'wb') as f:
-			pickle.dump(self._graph, f)
+    def save(self, filename):
+        with open(filename, 'wb') as f:
+            pickle.dump(self._graph, f)
 
 def load_map(name):
 	with open(name, 'rb') as f:
